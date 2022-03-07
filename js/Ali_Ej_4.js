b15598022fb69a34eea198327ecfac8af0aa8650
ali464haider <91870274+ali464haider@users.noreply.github.com>
2022-03-07 10:28:33 +0100

    Add files via upload

---
+++ js/Ali_Ej_4.js
@@ -0,0 +1,28 @@
+var nombres=["Ali","Alejandro","Brandom","Sergio"];
+function cargar(){
+    var myParent = document.body;
+    
+
+    var selectlist=document.createElement("select")
+    selectlist.id="myselect";
+
+    myParent.appendChild(selectlist);
+
+
+    for(i=0;i<nombres.length;i++){
+
+        var option=document.createElement("option");
+        option.value=i+1;
+        option.text=nombres[i];
+        selectlist.appendChild(option);
+        
+    }
+
+    
+}
+function clik(){
+    var nombre;
+    var valor=document.getElementById("myselect").value;
+    nombre=nombres[valor];
+    alert("El nombre seleccionado es "+nombre+" y es la option "+valor);
+}
